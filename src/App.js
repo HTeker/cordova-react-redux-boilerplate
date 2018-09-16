b2cbc575d36b7624d9cd1cb0814f3a44ba021bc3
DESKTOP-E71E6KE\Halil <h.teker.developer@gmail.com>
2018-09-16 09:31:54 +0200

    Example of utilizing Cordova Native API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            geo: {}
+        };
+    }
+
+    componentDidMount() {
+        if(window.cordova) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.setState({ geo: position.coords });
+            }, (error) => {
+
+            });
+        }
+    }
+
   render() {
     return (
       <div className="App">
@@ -12,6 +30,9 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+            Your location: { this.state.geo.latitude }, { this.state.geo.longitude }
         </p>
       </div>
     );
